b44b4f135feb030d1213fed715046828cf2965e0
Jumper <jonnyschoen29@gmail.com>
2025-12-16 10:08:37 -0500

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,9 @@
+function signup() {
+  localStorage.setItem("user", email.value);
+  window.location = "dashboard.html";
+}
+
+function login() {
+  localStorage.setItem("user", email.value);
+  window.location = "dashboard.html";
+}
